5a3b9548c02198b65530f1f60eb458d9e55c5c3d
code-abdulrehman <hy.abdulrahman2@gmail.com>
2024-08-17 10:51:11 +0500

    avatar

--- src/Components/SinglePost/index.jsx
+++ src/Components/SinglePost/index.jsx
@@ -28,15 +28,20 @@ function SinglePost({ postId }) {
                     <div className="postId">
                       <span className="text-blue-400"><b>#</b>PostId: &nbsp;</span>{data?.id || "0"}
                       </div>
-                    <div className="author">
-                      <span className="text-green-400"><b>#</b>Author: &nbsp;</span>{data?.userId || "0"}
-                      </div>
-            
                       </div>
                 
                       <div className="pre">
+                      
                         {data?.body || "Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus labore adipisci veritatis recusandae voluptatibus nihil obcaecati nulla consequuntur distinctio vero? Laborum numquam fugit tempora voluptatibus ex ratione mollitia architecto porro."}
-                      </div></>)}
+                      </div>
+                      <br />
+                      <div className="author flex justify-end">
+                    <span className="text-gray-400 font-mono font font-medium text-xs mr-1 flex items-end gap-2">Author <b className='text-gray-300'>@{data?.userId || "0"}
+                      </b></span> 
+                      <img src={`https://robohash.org/user${data.userId+data.id}.png`} className='h-12 w-12 rounded-full bg-yellow-500 overflow-hidden' alt="" />
+                      </div>
+            
+                      </>)}
 
   
     </div>
